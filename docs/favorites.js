e5a1d847f3ea72bdd637573f4e371457d1d102bc
Denys <reewd22@gmail.com>
2026-02-05 10:55:16 +0200

    Micro fix 2.2

--- docs/favorites.js
+++ docs/favorites.js
@@ -1,2 +1,2 @@
-import{s as t,i as e}from"./assets/main-C-q1t_Hd.js";document.addEventListener("DOMContentLoaded",()=>{t(),e()});
+import{d as t,s as e,i as s}from"./assets/main-ZCCaS1q5.js";document.addEventListener("DOMContentLoaded",()=>{t(),e(),s()});
 //# sourceMappingURL=favorites.js.map
