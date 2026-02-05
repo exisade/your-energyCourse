cfb6b006566475f764601a18df6411bbf85077f0
Denys <reewd22@gmail.com>
2026-02-05 10:45:59 +0200

    Fix image paths in favorites page

--- docs/favorites.js
+++ docs/favorites.js
@@ -1,2 +1,2 @@
-import{s as t,i as e}from"./assets/main-DIh6oRMx.js";document.addEventListener("DOMContentLoaded",()=>{t(),e()});
+import{s as t,i as e}from"./assets/main-C-q1t_Hd.js";document.addEventListener("DOMContentLoaded",()=>{t(),e()});
 //# sourceMappingURL=favorites.js.map
