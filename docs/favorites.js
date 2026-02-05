f497e53c2897c63545738ec7d20a975a9183f749
Denys <reewd22@gmail.com>
2026-02-05 11:17:41 +0200

    MicroFix 2.4

--- docs/favorites.js
+++ docs/favorites.js
@@ -1,2 +1,2 @@
-import{d as t,s as e,i as s}from"./assets/main-ZCCaS1q5.js";document.addEventListener("DOMContentLoaded",()=>{t(),e(),s()});
+import{d as t,s as e,i as s}from"./assets/main-DEx8t0Ld.js";document.addEventListener("DOMContentLoaded",()=>{t(),e(),s()});
 //# sourceMappingURL=favorites.js.map
